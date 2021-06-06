42a5c6a9dbe564b42db7f9c0a63622468e23529b
Richik19 <67725709+Richik19@users.noreply.github.com>
2021-06-06 19:06:29 +0530

    Add files via upload

---
+++ Task 1/Spider 1B.py
@@ -0,0 +1,32 @@
+#IMPORTS
+
+import math
+
+#INPUTS
+
+x = int(input())
+y = input()
+
+#CONSTRAINTS
+
+if x > 100000 and x < 1:
+    exit()
+if (math.log2(x)).is_integer() == False:
+    exit()
+if y.islower() == False:
+    exit()
+if x != len(y):
+    exit()
+
+#MAIN CODE
+    
+if x == 1:
+    print(0)
+else:
+    count = 0
+    while int(x) > 0:
+        x = x / 2
+        if y[:int(x)] == y[int(x):int(x*2)]:
+            count += 1
+    print(count)
+        
