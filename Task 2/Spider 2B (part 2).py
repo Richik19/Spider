7e3b9de528e24390c6d03f1bd34cd2646517ae58
Richik19 <67725709+Richik19@users.noreply.github.com>
2021-07-15 12:04:15 +0530

    Update and rename Spider 2B (part 2).txt to Spider 2B (part 2).py

---
+++ Task 2/Spider 2B (part 2).py
@@ -0,0 +1,34 @@
+#INPUTS
+n = int(input())
+q = int(input())
+l = []
+k = [i+1 for i in range(n)]
+for i in range(q):
+    a = input()
+    l += [a.split()]
+   
+#CONSTRAINTS
+if n < 1 or n > 10**5:
+    exit()
+if q < 1 or q > 10**5:
+    exit()
+for i in l:
+    if int(i[0]) < 1 or int(i[0]) > n:
+        exit()
+    if int(i[1]) < 1 or int(i[1]) > n:
+        exit()
+    if int(i[2]) > 10**9:
+        exit()
+
+#MAIN LOGIC
+p = [0 for i in range(n)]
+for i in range(q):
+    x, y = int(l[i][0]),int(l[i][1])
+    p[x-1] += int(l[i][2])
+    if y < n:
+        p[y] -= int(l[i][2])
+for i in range(1,n):
+    p[i] += p[i-1]
+for i in range(n):
+    p[i] += k[i]
+print(max(p))
